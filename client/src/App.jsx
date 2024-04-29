5c4a2bfd7a879c525efce447a853389a73deeacc
zephyr <jafaribrownsha@gmail.com>
2024-04-29 01:08:25 -0400

    added new route for the landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import Home from "./views/Home";
 import Register from "./views/Register";
 import Login from "./views/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./views/landingPage";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route
+            path="/landingPage"
+            element={<LandingPage></LandingPage>}
+          ></Route>
           <Route path="/home" element={<Home></Home>}></Route>
           <Route path="/signUp" element={<Register></Register>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>

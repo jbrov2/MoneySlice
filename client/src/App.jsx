89850cf7a993561a9fa6945c5ff833d22af450e4
zephyr <jafaribrownsha@gmail.com>
2024-04-30 19:43:51 -0400

    made a new view for app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,21 @@ import "./App.css";
 import Home from "./views/Home";
 import Register from "./views/Register";
 import Login from "./views/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./views/landingPage";
+import CreateAPie from "./views/createBudget";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/createAPie" element={<CreateAPie></CreateAPie>}></Route>
           <Route
             path="/landingPage"
             element={<LandingPage></LandingPage>}
@@ -17,6 +24,7 @@ function App() {
           <Route path="/home" element={<Home></Home>}></Route>
           <Route path="/signUp" element={<Register></Register>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="" element={<Navigate to="/landingPage" />} />
         </Routes>
       </Router>
     </>
---
+++ client/src/views/createBudget.jsx
@@ -0,0 +1,28 @@
+function CreateAPie() {
+  return (
+    <>
+      <header className="header">
+        <nav className="navbar">
+          <ul className="nav-menu">
+            <li className="nav-item" id="services">
+              Services
+            </li>
+            <li className="nav-item" id="Logo">
+              Money Slice
+            </li>
+            <li className="nav-item" id="login-btn">
+              Login
+            </li>
+          </ul>
+          <div className="hamburger">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default CreateAPie;

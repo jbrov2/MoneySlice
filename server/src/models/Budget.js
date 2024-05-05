4f1d3652d266caf9db8004d20788ff82c864fe07
zephyr <jafaribrownsha@gmail.com>
2024-05-05 19:15:00 -0400

    adding user id

--- server/src/models/Budget.js
+++ server/src/models/Budget.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
-
+const User = require("../models/User");
 const Schema = mongoose.Schema;
 // const ObjectId = Schema.ObjectId;
 
 const BudgetSchema = new Schema({
+  User: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   Category: String,
   Budgeted_Amount: Number,
   Actual_Spending: Number,

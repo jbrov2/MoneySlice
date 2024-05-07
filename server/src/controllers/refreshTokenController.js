77c07cded7e634d768ec3572fb37eb8530bb5a7a
zephyr <jafaribrownsha@gmail.com>
2024-05-07 19:51:38 -0400

    making refresh controller

---
+++ server/src/controllers/refreshTokenController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const handleLogin = async (req, res) => {
+  const { userName, password } = req.body;
+  if (!userName || !password)
+    return res
+      .status(400)
+      .json({ message: "Username and password is required." });
+
+  const foundUser = await User.findOne({ userName: userName }).exec();
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+  // Evalute password
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    //create JWTs
+    const accessToken = jwt.sign(
+      { userName: foundUser.userName },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1h" }
+    );
+    const refreshToken = jwt.sign(
+      { userName: foundUser.userName },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    //saving refresh token
+
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };

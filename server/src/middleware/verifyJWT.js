c5d238459175c0749a78c952aed28a04316749c7
zephyr <jafaribrownsha@gmail.com>
2024-05-07 21:36:08 -0400

    made it more efficient

--- server/src/middleware/verifyJWT.js
+++ server/src/middleware/verifyJWT.js
@@ -2,10 +2,10 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyjwt = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.sendStatus(401);
-  console.log(authHeader); // Bearer token
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
+  console.log(token);
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err || !decoded) {
       console.error("JWT verification failed or token is not defined: ", err);

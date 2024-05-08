23aababb994c02ed6a09c0a2b4f56f415bc6ce2f
zephyr <jafaribrownsha@gmail.com>
2024-05-07 20:51:51 -0400

    edited refresh controller had some commands in there for testing purposes

--- server/src/controllers/refreshTokenController.js
+++ server/src/controllers/refreshTokenController.js
@@ -1,26 +1,47 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 
-const handleRefreshToken = (req, res) => {
+const handleRefreshToken = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
   const refreshToken = cookies.jwt;
 
-  const foundUser = User.findOne({ refreshToken }).exec();
-  if (!foundUser) return res.sendStatus(403); //Forbidden
-  // Evalute jwt
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || foundUser.userName !== decoded.userName)
-      return res.sendStatus(403); //forbidden
+  try {
+    //Checking refreshToken Value
+    console.log("Received refreshToken:", refreshToken);
 
-    const accessToken = jwt.sign(
-      { userName: decoded.userName },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "5m" }
+    const foundUser = await User.findOne({ refreshToken }).exec();
+    if (!foundUser) {
+      console.error("Failed to find the user");
+      return res.sendStatus(403); //Forbidden
+    }
+
+    // Evalute jwt
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err || foundUser.userName !== decoded.userName) {
+          console.error(
+            "JWT verification failed or token is not defined: ",
+            err
+          );
+          return res.sendStatus(403); //forbidden
+        }
+        const accessToken = jwt.sign(
+          { userName: decoded.userName },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "5m" }
+        );
+        res.json({ accessToken });
+      }
     );
-    res.json({ accessToken });
-  });
+  } catch (error) {
+    console.log("Error:", error);
+    res.sendStatus(500); //internal error
+  }
 };
 
 module.exports = { handleRefreshToken };

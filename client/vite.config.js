3662e45a8185bd38440d3ce0551023fadbafaeb2
zephyr <jafaribrownsha@gmail.com>
2024-05-16 21:11:09 -0400

    added for modules

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      //Enable camel case names
+      localsConvention: "camelCase",
+      //scopes the class names to avoid conflicts
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+  },
+});

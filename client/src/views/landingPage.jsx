2df2e93af823b3199aef67619446e3dc71b11bf7
zephyr <jafaribrownsha@gmail.com>
2024-05-19 17:18:17 -0400

    fixing formatting and adding necessary items

--- client/src/views/landingPage.jsx
+++ client/src/views/landingPage.jsx
@@ -1,69 +1,47 @@
 import { useNavigate } from "react-router-dom";
-// eslint-disable-next-line no-unused-vars
 import styles from "../styles/landingPage.module.css";
 
 function LandingPage() {
-  const link = useNavigate();
+  const navigate = useNavigate();
 
   function handleLoginPage() {
-    link("/login");
+    navigate("/login");
   }
 
   function handleSignUpPage() {
-    link("/signUp");
+    navigate("/signUp");
   }
 
   return (
-    <>
-      {/* <header className="header">
-        <nav className="navbar">
-          <ul className="nav-menu">
-            <li className="nav-item" id="services">
-              Services
-            </li>
-            <li className="nav-item" id="Logo">
-              Money Slice
-            </li>
-            <li className="nav-item" id="login-btn" onClick={handleLoginPage}>
-              Login
-            </li>
-          </ul>
-          <div className="hamburger">
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-        </nav>
-      </header> */}
-      <div className={`${styles.wrapper}`}>
-        <section className={`${styles.main}`}>
-          <div className={`${styles.title}`}>
-            <h1>
-              Money <span className={`${styles.splice}`}>Slice</span>
-            </h1>
-          </div>
-
-          <div className={`${styles.landing}`}>
-            <h3 className={`${styles.landing_text}`}>Your go to</h3>
+    <div className={styles.wrapper}>
+      <section className={styles.main}>
+        <div className={styles.title}>
+          <h1>
+            Money <span id={styles.splice}>Slice</span>
+          </h1>
+        </div>
+        <div className={styles.landing}>
+          <div className={styles.landing_block}>
+            <h3 className={styles.landing_text}>Your go to</h3>
             <h3
               className={`${styles.landing_text}`}
               id={`${styles.special_text}`}
             >
               pie chart
             </h3>
-            <h3 className={`${styles.landing_text}`}>budgeting app</h3>
+            <h3 className={styles.landing_text}>budgeting app</h3>{" "}
+            <section className={styles.buttons}>
+              <button className={styles.login_btn} onClick={handleLoginPage}>
+                Login
+              </button>
+              <button className={styles.signUp_btn} onClick={handleSignUpPage}>
+                Sign Up
+              </button>
+            </section>
           </div>
-          <button
-            className={`${styles.signUp_btn}`}
-            onClick={handleSignUpPage}
-          ></button>
-          <button
-            className={`${styles.login_btn}`}
-            onClick={handleLoginPage}
-          ></button>
-        </section>
-      </div>
-    </>
+        </div>
+      </section>
+    </div>
   );
 }
 

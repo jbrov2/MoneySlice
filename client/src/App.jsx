54e119175becb1b622af9152640bc351dcdf7d9a
zephyr <jafaribrownsha@gmail.com>
2024-05-28 23:02:22 -0400

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,9 @@ import {
 import LandingPage from "./views/landingPage";
 import CreateAPie from "./views/createBudget";
 import ViewAPie from "./views/viewBudget";
+import UpdateAPie from "./views/updateBudget";
+import DeleteApie from "./views/deleteBudget";
+import LogoutPage from "./views/LogoutPage";
 // Supports weights 100-800
 import "@fontsource-variable/sora";
 import "@fontsource/krona-one";
@@ -20,6 +23,9 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/logout" element={<LogoutPage></LogoutPage>}></Route>
+          <Route path="/smashAPie" element={<DeleteApie></DeleteApie>}></Route>
+          <Route path="/updateAPie" element={<UpdateAPie></UpdateAPie>}></Route>
           <Route path="/createAPie" element={<CreateAPie></CreateAPie>}></Route>
           <Route path="/viewAPie" element={<ViewAPie></ViewAPie>} />
           <Route

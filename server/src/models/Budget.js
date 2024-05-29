c8178d96fa31eb922db1d7e8e71ccd20780bc2c0
zephyr <jafaribrownsha@gmail.com>
2024-05-29 17:54:05 -0400

    changing budget and user

--- server/src/models/Budget.js
+++ server/src/models/Budget.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+const UserModel = require("./User");
 
 const Schema = mongoose.Schema;
 // const ObjectId = Schema.ObjectId;
 
 const BudgetSchema = new Schema({
-  Category: String,
+  Category: { type: String, required: true, unique: true },
+  Item: [
+    {
+      Name: { type: String, required: true },
+      Amount_Spent: { type: Number, required: true },
+      ID: { type: Number, required: true },
+    },
+  ],
   Budgeted_Amount: Number,
-  Actual_Spending: Number,
+  Actual_Spending: { type: Number, default: 0 },
   Remaining_Budget: {
     type: Number,
     default: function () {
@@ -14,9 +22,25 @@ const BudgetSchema = new Schema({
       return this.Budgeted_Amount || 0;
     },
   },
+  User: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   // id: mongoose.Schema.Types.ObjectId,
+});
+
+//Pre-save hook to calculate actual spending
+BudgetSchema.pre("save", function (next) {
+  //Calculate the sum of Amount_Spent in the Item Array
+  this.Actual_Spending = this.Item.reduce((total, item) => {
+    return total + item.Amount_Spent;
+  }, 0);
+  this.Remaining_Budget = this.Budgeted_Amount - this.Actual_Spending;
+
+  next();
 });
 
 const BudgetModel = mongoose.model("Budget", BudgetSchema);
 
-module.exports = BudgetModel;
+module.exports = { BudgetModel };
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -16,27 +16,9 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
-  refreshToken: String,
-  budgets: [
-    {
-      Category: String,
-      Budgeted_Amount: Number,
-      Actual_Spending: Number,
-      Remaining_Budget: {
-        type: Number,
-        default: function () {
-          return this.Budgeted_Amount || 0;
-        },
-      },
-    },
-  ],
-});
+  AssignedBudgets: [{ type: Schema.Types.ObjectId, ref: "Budget" }],
 
-UserSchema.pre("save", function (next) {
-  this.budgets.forEach((budget) => {
-    budget.Remaining_Budget = budget.Budgeted_Amount - budget.Actual_Spending;
-  });
-  next();
+  refreshToken: String,
 });
 
 const UserModel = mongoose.model("User", UserSchema);

93741b724b2ce13ca5481fb97f3dbb234cdfdcf9
zephyr <jafaribrownsha@gmail.com>
2024-06-01 13:50:37 -0400

    filling out the code for viewing a budget

--- client/src/views/viewBudget.jsx
+++ client/src/views/viewBudget.jsx
@@ -1,5 +1,87 @@
+import styles from "../styles/viewABudget.module.css";
+import { useNavigate } from "react-router-dom";
+
 function ViewAPie() {
-  return <></>;
+  const navigate = useNavigate();
+
+  const handleHomePage = () => navigate("/home");
+  const handleCreatePage = () => navigate("/createAPie");
+  const handleViewPage = () => navigate("/viewAPie");
+  const handleUpdatePage = () => navigate("/updateApie");
+  const handleDeletePage = () => navigate("/smashAPie");
+  const handleLogout = () => navigate("/logout");
+
+  async function seeBudget() {
+    const token = localStorage.getItem("accessToken");
+
+    const response = await fetch("http://localhost:5000/budget", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.main}>
+          <section className={styles.sidebar}>
+            <div className={styles.logo}>
+              <h2>MS</h2>
+            </div>
+
+            <div className={styles.selection}>
+              <div className={styles.sidescreen_links}>
+                <ul>
+                  <li className={styles.links} onClick={handleHomePage}>
+                    Home
+                  </li>
+                  <li className={styles.links} id={styles.static}>
+                    Budgets
+                  </li>
+                  <li
+                    className={styles.links}
+                    id={styles.chosen_link}
+                    onClick={handleViewPage}
+                  >
+                    View
+                  </li>
+                  <li className={styles.links} onClick={handleCreatePage}>
+                    {" "}
+                    Make
+                  </li>
+                  <li className={styles.links} onClick={handleUpdatePage}>
+                    Update
+                  </li>
+                  <li className={styles.links} onClick={handleDeletePage}>
+                    Delete
+                  </li>
+                  <li
+                    className={styles.links}
+                    id={styles.logout_link}
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <div className={styles.welcome_banner}>
+            <h1 className={styles.h1_text}>
+              Welcome to <span className={styles.custom_text}>View a Pie</span>!
+            </h1>
+          </div>
+          <section className={styles.prompt}>
+            <h2 className={styles.prompt_h2}>Choose a PieChart to View</h2>
+          </section>
+          <section className={styles.PieChart_Selector}></section>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ViewAPie;

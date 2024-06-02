f23d5c983ec7332594d0752d47ecdcf6cd58d8f4
zephyr <jafaribrownsha@gmail.com>
2024-06-02 16:19:54 -0400

    got rid of unnecessary code

--- server/src/index.js
+++ server/src/index.js
@@ -35,32 +35,10 @@ app.use("/signUp", require("./routes/signUp"));
 app.use("/login", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
+
 app.use(verifyJWT);
 app.use("/user", require("./routes/users"));
 app.use("/budget", require("./routes/budget"));
-app.get("/login", cors(), (req, res) => {});
-// app.post("/login", async (req, res) => {
-//   const { userName, password } = req.body;
-
-//   try {
-//     // const existingUser = await Login.findOne({ userName });
-//     const existingUser = await Login.find({ userName, password });
-//     //add some logic where it checks for username and password before moving to next page
-//     //Watch JWT video
-
-//     if (existingUser) {
-//       res.json("exist");
-//     } else {
-//       res.json("does not exist");
-//     }
-//   } catch (e) {
-//     res.json("not exist");
-//   }
-// });
-
-app.get("/signUp", (req, res) => {
-  res.send("Welcome to the signUp page");
-});
 
 mongoose.connect(url).then(() => {
   app.listen(PORT, () => {

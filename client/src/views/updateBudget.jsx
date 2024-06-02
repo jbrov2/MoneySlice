eb4150ca54702fee39affde69d962d8e369dc2c1
zephyr <jafaribrownsha@gmail.com>
2024-06-02 19:33:02 -0400

    adding jsx for budget

--- client/src/views/updateBudget.jsx
+++ client/src/views/updateBudget.jsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/updateABudget.module.css";
+import { useEffect, useState } from "react";
+import { Chart as ChartJs, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
+ChartJs.register(ArcElement, Tooltip, Legend);
 function UpdateBudget() {
   const navigate = useNavigate();
 
@@ -10,6 +14,143 @@ function UpdateBudget() {
   const handleUpdatePage = () => navigate("/updateApie");
   const handleDeletePage = () => navigate("/smashAPie");
   const handleLogout = () => navigate("/logout");
+
+  //for viewing the budget
+  const [budget, setBudget] = useState([]);
+  const [selectedBudget, setSelectedBudget] = useState(null);
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "Spending",
+        data: [],
+        background: [
+          "2F4B26",
+          "3E885B",
+          "85BDA6",
+          "BEDCFE",
+          "C0D7BB",
+          "F3D3BD",
+        ],
+      },
+    ],
+  });
+  //editing budget
+  const [Budget_Amounted, setBudget_Amounted] = useState("");
+  const [Category, setCategory] = useState("");
+  const [items, setItems] = useState([]);
+  const [itemName, setItemName] = useState("");
+  const [itemAmountSpent, setItemAmountSpent] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  //after everything is said and done this effect will allow you to see the budget
+  useEffect(() => {
+    seeBudget();
+  }, []);
+
+  async function seeBudget() {
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await fetch("http://localhost:5000/budget", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      console.log("Fetched budget data:", data);
+      setBudget(data);
+    } catch (error) {
+      console.error("Error fetching budget", error);
+    }
+  }
+
+  function handleBoxClick(budget) {
+    console.log("Selected budget:", budget); // Debugging statement
+    setSelectedBudget(budget);
+    setChartData({
+      labels: budget.items.map((item) => item.name),
+      datasets: [
+        {
+          label: "Spending",
+          data: budget.items.map((item) => item.amountSpent),
+          backgroundColor: [
+            "#2F4B26",
+            "#3E885B",
+            "#85BDA6",
+            "#BEDCFE",
+            "#C0D7BB",
+            "#F3D3BD",
+          ],
+        },
+      ],
+    });
+  }
+
+  function handleEditButton() {
+    setIsEditing(true);
+    setCategory(selectedBudget.category);
+    setBudget_Amounted(selectedBudget.budgetedAmount);
+    setItems(selectedBudget.items);
+  }
+
+  async function handleSaveEdit() {
+    const token = localStorage.getItem("accessToken");
+
+    const Budgeted_Amount = parseFloat(Budget_Amounted);
+    const requestBody = {
+      Category,
+      Budgeted_Amount: Budgeted_Amount,
+      Item: items,
+    };
+    try {
+      const response = await fetch("http://localhost:5000/budget", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!response.ok) {
+        console.error(`Error: ${response.status} ${response.statusText}`);
+        return;
+      }
+
+      const data = await response.json();
+    } catch (error) {
+      console.error.eror("Failed to Parse JSON response:", error);
+    }
+  }
+
+  function handleCancelEdit() {
+    setIsEditing(false);
+  }
+  //to set chart with new data
+  useEffect(() => {
+    setChartData({
+      labels: items.map((item) => item.name),
+      datasets: [
+        {
+          label: "Spending",
+          data: items.map((item) => item.amount),
+          backgroundColor: [
+            "#2F4B26",
+            "#3E885B",
+            "#85BDA6",
+            "#BEDCFE",
+            "#C0D7BB",
+            "#F3D3BD",
+          ],
+        },
+      ],
+    });
+  }, [items]);
+
+  function handleClosePopup() {
+    setSelectedBudget(null);
+  }
 
   return (
     <>
@@ -57,7 +198,94 @@ function UpdateBudget() {
               </div>
             </div>
           </section>
-          <section className={styles.main_section}></section>
+          <div className={styles.welcome_banner}>
+            <h1 className={styles.h1_text}>
+              Welcome to{" "}
+              <span className={styles.custom_text}>Update a Pie</span> !
+            </h1>
+          </div>
+          <section className={styles.main_section}>
+            <div className={styles.prompt}>
+              <h2 className={styles.prompt_h2}>
+                Choose a <span className={styles.custom_text}>PieChart</span> to
+                update:
+              </h2>
+              {budget.length === 0 ? (
+                <p className={styles.no_budget}>
+                  No budgets are available. Please create a budget.
+                </p>
+              ) : (
+                <div className={styles.budget_boxes}>
+                  {" "}
+                  {budget.map((budget, i) => (
+                    <div
+                      key={i}
+                      className={styles.budget_box}
+                      onClick={() => handleBoxClick(budget)}
+                    >
+                      <h3>{budget.category}</h3>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </section>
+          {selectedBudget && (
+            <div className={styles.popup}>
+              <div className={styles.popup_content}>
+                <h2>{selectedBudget.category}</h2>
+                <p>Budgeted Amount: {selectedBudget.budgetedAmount}</p>
+                <p>Actual Spending: {selectedBudget.actualSpending}</p>
+                <p>Amount Remaining: {selectedBudget.remainingBudget}</p>
+                <h3 className={styles.item_h3}>Items</h3>
+                <div className={styles.chart_container}>
+                  <Pie data={chartData}></Pie>
+                </div>
+                <div className={styles.button_holder}>
+                  <button
+                    className={styles.edit_button}
+                    onClick={handleEditButton}
+                  >
+                    Edit Budget
+                  </button>
+                  <button
+                    className={styles.close_button}
+                    onClick={handleClosePopup}
+                  >
+                    Close
+                  </button>
+                </div>
+                {isEditing && (
+                  <div className={styles.popup_edit}>
+                    <div className={styles.pop_edit_content}>
+                      <h2>Edit Content</h2>
+                      <div className={styles.prevSection}>
+                        <p>Category: {selectedBudget.category}</p>
+                        <p>Budgeted Amount: {selectedBudget.remainingBudget}</p>
+                        <p>Name: {items[items.length - 1].name}</p>
+                        <p>Amount: {items[items.length - 1].amount}</p>
+                      </div>
+                      <div className={styles.button_holder}>
+                        <button
+                          className={styles.save_button}
+                          onClick={handleSaveEdit}
+                        >
+                          {" "}
+                          Save
+                        </button>
+                        <button
+                          className={styles.cancel_button}
+                          onClick={handleCancelEdit}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>

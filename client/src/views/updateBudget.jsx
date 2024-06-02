16b64aa2a696c25861fc96877c4a541c6bf21a4e
zephyr <jafaribrownsha@gmail.com>
2024-06-02 16:19:39 -0400

    workinh on update jsx

--- client/src/views/updateBudget.jsx
+++ client/src/views/updateBudget.jsx
@@ -1,9 +1,67 @@
-function updateBudget() {
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/updateABudget.module.css";
+
+function UpdateBudget() {
+  const navigate = useNavigate();
+
+  const handleHomePage = () => navigate("/home");
+  const handleCreatePage = () => navigate("/createAPie");
+  const handleViewPage = () => navigate("/viewAPie");
+  const handleUpdatePage = () => navigate("/updateApie");
+  const handleDeletePage = () => navigate("/smashAPie");
+  const handleLogout = () => navigate("/logout");
+
   return (
     <>
-      <h1>Hello World</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.main}>
+          <section className={styles.sidebar}>
+            <div className={styles.logo}>
+              <h2>MS</h2>
+            </div>
+
+            <div className={styles.selection}>
+              <div className={styles.sidescreen_links}>
+                <ul>
+                  <li className={styles.links} onClick={handleHomePage}>
+                    Home
+                  </li>
+                  <li className={styles.links} id={styles.static}>
+                    Budgets
+                  </li>
+                  <li
+                    className={styles.links}
+                    id={styles.chosen_link}
+                    onClick={handleViewPage}
+                  >
+                    View
+                  </li>
+                  <li className={styles.links} onClick={handleCreatePage}>
+                    {" "}
+                    Make
+                  </li>
+                  <li className={styles.links} onClick={handleUpdatePage}>
+                    Update
+                  </li>
+                  <li className={styles.links} onClick={handleDeletePage}>
+                    Delete
+                  </li>
+                  <li
+                    className={styles.links}
+                    id={styles.logout_link}
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <section className={styles.main_section}></section>
+        </div>
+      </div>
     </>
   );
 }
 
-export default updateBudget;
+export default UpdateBudget;

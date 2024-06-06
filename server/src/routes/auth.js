3b6fd60ff85b8199bc7191491caf09b753b6d2fe
zephyr <jafaribrownsha@gmail.com>
2024-06-05 22:53:17 -0400

    new routes in auth

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const loginLimitter = require("../middleware/loginLimiter");
 
-router.post("/", authController.handleLogin);
+router.route("/").post(loginLimitter, authController.handleLogin);
+
+router.route("/refresh").get(authController.handleRefreshToken);
+
+router.route("/logout").post(authController.handleLogout);
 
 module.exports = router;

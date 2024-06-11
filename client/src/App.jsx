aa8a107158114a533619feb1401a47528369899c
zephyr <jafaribrownsha@gmail.com>
2024-06-11 19:58:15 -0400

    got rid of basename

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
 
   return (
     <>
-      <Router basename="/MoneySlice/">
+      <Router>
         <Routes>
           <Route path="/logout" element={<LogoutPage />} />
           <Route path="/smashAPie" element={<DeleteApie />} />
